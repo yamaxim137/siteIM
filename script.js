5529660cff06c5bf34f812f2827417218342fa2d
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-02-26 18:11:10 +0300

    база

---
+++ script.js
@@ -0,0 +1,33 @@
+const catalogProduct = [
+{
+    id: '',
+    name: 'Audi a8',
+    price: '199888',
+    img: './img/audi_a8.jpg',
+},
+{
+    id: '',
+    name: 'BMW i8',
+    price: '171888',
+    img: './img/bmw i8 .jpg',
+},
+{
+    id: '',
+    name: 'VWagen',
+    price: '137838',
+    img: './img/dAuto.jpg',
+},
+{
+    id: '',
+    name: 'Reno',
+    price: '3456',
+    img: './img/bmw i8 .jpg',
+},
+{
+    id: '',
+    name: 'Lada',
+    price: '55444',
+    img: './img/dAuto.jpg',
+}
+];
+
