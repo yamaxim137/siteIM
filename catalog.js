84ebbab5f611de531e2742fb5186cb9ea3a12ed2
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-02-26 18:57:46 +0300

    catalog +

--- catalog.js
+++ catalog.js
@@ -5,11 +5,60 @@ class AllProducts{
         this.createProduct();
     }
 
+    // <div class="item">
+    //         <div class="name">product 1</div>
+    //         <div class="img"></div>
+    //         <div class="price">199</div>
+    //         <button class="btn">Купить</button>
+    // </div>
+
     createProduct(){
+        let wraper = document.createElement("slot");
+        for(let i=0;i<this.catalogProduct.length; i++){
+            let item = this.getProductItem({
+                tagName:"div",
+                className:"item"
+            })
+            let name = this.getProductItem({
+                tagName:"div",
+                className:"name",
+                textName: this.catalogProduct[i].name
+            })
+            let img = this.getProductItem({
+                tagName:"div",
+                className:"img",
+                backgroundImg: `url(${this.catalogProduct[i].img})`
+            })
+            let price = this.getProductItem({
+                tagName:"div",
+                className:"price",
+                textName: this.catalogProduct[i].price
+            })
+            let btn = this.getProductItem({
+                tagName:"button",
+                className:"btn",
+                textName: 'Купить'
+            })
+
+            item.appendChild(name);
+            item.appendChild(img);
+            item.appendChild(price);
+            item.appendChild(btn);
+
+            wraper.appendChild(item);
+        }
+
+        this.containerProducts.appendChild(wraper);
 
     }
 
-    getProductItem(){
-        
+    getProductItem(card){
+        let element = document.createElement(card.tagName);
+        if('className' in card){element.setAttribute('class', card.className)}
+        if('textName' in card){element.innerHTML =  card.textName;}
+        if('backgroundImg' in card){element.style.backgroundImage = card.backgroundImg;}
+        return element;
     }
 }
+
+let allProducts = new AllProducts('.container-product', catalogProduct);
