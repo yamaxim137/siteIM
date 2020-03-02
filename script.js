683b96fe53b87b35401fa930d0789ca31bf21fa3
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-03-02 15:45:49 +0300

    acc

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ const catalogProduct = [
     name: 'Audi a8',
     price: 199888,
     img: './img/audi_a8.jpg',
+    text: '1\nпервый продукт - Ауди а8'
 },
 {
     id: 'product',
     name: 'BMW i8',
     price: 171888,
     img: './img/bmw i8 .jpg',
+    text: '2\nвторая машина'
 },
 {
     id: 'product3',
     name: 'VWagen',
     price: 137838,
     img: './img/dAuto.jpg',
+    text: '3\nещё один продукт'
 },
 {
     id: 'product4',
     name: 'Reno',
     price: 3456,
     img: './img/bmw i8 .jpg',
+    text: '4\nчетвёрочка'
 },
 {
     id: 'product5',
     name: 'Lada',
     price: 55444,
     img: './img/dAuto.jpg',
+    text:'5\nпятая'
 }
 ]
