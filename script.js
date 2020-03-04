a722aecdcd3e8baa0b6b9b0effc7c89eb9785d29
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-03-04 17:17:34 +0300

    мелочи

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const catalogProduct = [
     text: '1\nпервый продукт - Ауди а8'
 },
 {
-    id: 'product',
+    id: 'product2',
     name: 'BMW i8',
     price: 171888,
     img: './img/bmw i8 .jpg',
